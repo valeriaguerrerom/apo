a48310444a41f167dae36decc181fe920fb16917
Maria Vallejo <maalvallejo123@umariana.edu.co>
2023-10-13 21:03:11 -0500

    Cambio2:// declara el metodo main

--- areadelcirculo.java
+++ areadelcirculo.java
@@ -1,6 +1,7 @@
 public class AreaCirculo {
     //Esta línea declara una clase pública llamada 
     public static void main(String[] args) {
+        //Esta línea declara el método main(), que es el método de entrada de todas las aplicaciones Java. El método main() es el primer método que se ejecuta cuando se inicia una aplicación Java.
         double radio = 5.0;
         double area = Math.PI * Math.pow(radio, 2);
         System.out.println ("El área del círculo es: " + area);
