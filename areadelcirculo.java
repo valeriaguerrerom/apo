894d522b18c9a549451bddc0889280812bb03ac0
Maria Vallejo <maalvallejo123@umariana.edu.co>
2023-10-13 20:43:27 -0500

    Cambio1: //codigo areadelcirculo

---
+++ areadelcirculo.java
@@ -0,0 +1,7 @@
+public class AreaCirculo {
+    public static void main(String[] args) {
+        double radio = 5.0;
+        double area = Math.PI * Math.pow(radio, 2);
+        System.out.println ("El área del círculo es: " + area);
+    }
+}
