f53682301c7b01a889f6d2435693a37523367cec
Maria Vallejo <maalvallejo123@umariana.edu.co>
2023-10-13 21:40:05 -0500

    Cambio10: //conclusion 2 final

--- areadelcirculo.java
+++ areadelcirculo.java
@@ -12,5 +12,7 @@ public class AreaCirculo {
         //Luego, calcula el área del círculo utilizando la fórmula área = π * radio^2. Finalmente, imprime el área del círculo en la consola.
     }
     //conclusiones 
+   //La comprension de la sintaxis basica de java es esencial para poder escribir este tipo de codigos,en este caso la comprension de las declaraciones de variables
     // El codigo java es una herramienta poderosa que se puede utilizar para realizar una variedad de tareas, en este caso el codigo se utilizo para calcular el area de un circulo
 }
+
