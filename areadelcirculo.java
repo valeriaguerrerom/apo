d84329562bf023a1d4dc709b6c593f714ff091c7
Maria Vallejo <maalvallejo123@umariana.edu.co>
2023-10-13 21:24:34 -0500

    Cambio5:// explica la formula del area

--- areadelcirculo.java
+++ areadelcirculo.java
@@ -3,8 +3,10 @@ public class AreaCirculo {
     public static void main(String[] args) {
         //Esta línea declara el método main(), que es el método de entrada de todas las aplicaciones Java. El método main() es el primer método que se ejecuta cuando se inicia una aplicación Java.
         double radio = 5.0;
-        //Esta línea declara una variable de tipo double llamada radio y le asigna el valor 5.0.
+        //Esta línea declara una variable de tipo.double llamada radio y le asigna el valor 5.0.
         double area = Math.PI * Math.pow(radio, 2);
+        //Esta linea calcula el area del circulo utilizando la formula área = π * radio^2
+        
         System.out.println ("El área del círculo es: " + area);
     }
 }
