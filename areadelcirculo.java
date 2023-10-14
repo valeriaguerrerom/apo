44f68355b70b18ce3ebedd30fa925f098a67dda2
Maria Vallejo <maalvallejo123@umariana.edu.co>
2023-10-13 20:52:58 -0500

    Cambio2://codigo areadelcirculo

--- areadelcirculo.java
+++ areadelcirculo.java
@@ -1,4 +1,5 @@
 public class AreaCirculo {
+    //Esta línea declara una clase pública llamada 
     public static void main(String[] args) {
         double radio = 5.0;
         double area = Math.PI * Math.pow(radio, 2);
