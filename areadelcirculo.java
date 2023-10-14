509ad0f4f5281d79880c520348f79d47341bfaac
Maria Vallejo <maalvallejo123@umariana.edu.co>
2023-10-13 21:15:29 -0500

    Cambio4: // se explica la linea double

--- areadelcirculo.java
+++ areadelcirculo.java
@@ -3,6 +3,7 @@ public class AreaCirculo {
     public static void main(String[] args) {
         //Esta línea declara el método main(), que es el método de entrada de todas las aplicaciones Java. El método main() es el primer método que se ejecuta cuando se inicia una aplicación Java.
         double radio = 5.0;
+        //Esta línea declara una variable de tipo double llamada radio y le asigna el valor 5.0.
         double area = Math.PI * Math.pow(radio, 2);
         System.out.println ("El área del círculo es: " + area);
     }
